8c3ff0cafd3e67874143f65248f80be606f17d6b
KingoCor <minaew.leshas@gmail.com>
2021-09-25 21:39:52 +0300

    добавил шапку

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import "./App.css";
+
 const App = () => (
 
-	<p> Здесь будет сайт </p>
+	<div>
+		<Header />
+
+	</div>
 
   );
+
+
+const Header = () => (
+	<header>
+
+		<h1>Formed</h1>
+		
+		<div>
+			<p> Поиск по названию <input></input> </p>
+
+			<button>Поиск по симптомам</button>
+		</div>
+
+	</header>
+);
 
 export default App;
