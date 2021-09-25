acec11fe07ed1f36e9a4ed12c6f75d02dbdb754e
KingoCor <57768209+KingoCor@users.noreply.github.com>
2021-09-26 01:40:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,13 +14,12 @@ const Header = () => (
 	<header>
 
 		<h1>Formed</h1>
-		
+	
 		<div>
 			<p> <input></input>  <button>Поиск</button> </p>
 
 			<button>Поиск по симптомам</button>
 		</div>
-
 	</header>
 );
 
