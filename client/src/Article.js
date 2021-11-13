2b2732274cec83201460382a2c6090fe35e645aa
KingoCor <aleksei.minaef@yandex.ru>
2021-11-13 22:13:08 +0300

    добавлено всё необходимое для работы сайтаы сайты

--- client/src/Article.js
+++ client/src/Article.js
@@ -1,10 +1,16 @@
 import React from "react"
 import "./App.css";
+import {useLocation} from "react-router-dom";
+
+const location = useLocation();
+const id = location.pathname.split("/");
+
+import article from "./articles/"+id[id.length-1];
 
 const Article = () => (
 
 	<div>
-		<p>Здесь будет статья</p>
+		<iframe src={article}/>
 	</div>
 
   );
--- client/src/Search.js
+++ client/src/Search.js
@@ -1,22 +1,25 @@
 import React from "react"
 import "./App.css";
 import {articles} from "./App.js"
+import {Link} from "react-router-dom";
 
 const getArticlesPreview = (articles_json) => {
 	if (articles.message=="articles founded")
 	{
 		return(articles_json.articles.map(article => (
-			<button className="article-preview">
-				<div align="left">
-					<h1>{article.name}</h1>
-					<p>{article.tags}</p>
-				</div>
+            <Link to={"/Article/"+article.file_name}>
+                <blockquote>
+                    <div align="left">
+                        <h1>{article.name}</h1>
+                        <p>{article.tags}</p>
+                    </div>
 
-				<div align="right">
-					<p>Просмотры: {article.views}</p>
-					<p>Рейтинг: {article.rating}</p>
-				</div>
-			</button>
+                    <div align="right">
+                        <p>Просмотры: {article.views}</p>
+                        <p>Рейтинг: {article.rating}</p>
+                    </div>
+                </blockquote>
+            </Link>
 		)))
 	} else {
 		return(<button align="center"><h1>Ничего не найдено</h1></button>)
